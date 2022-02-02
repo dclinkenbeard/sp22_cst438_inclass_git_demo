7f574d614f4caf0da2c793f2bea7b593965263f5
dclinkenbeard <dclinkenbeard@csumb.edu>
2022-02-01 17:16:35 -0800

    Added a book object to show branches
    Added a constructor
    
    added a comment

---
+++ Book.java
@@ -0,0 +1,15 @@
+/**
+ * Author: drew
+ * Date: 1 - Feb - 2022
+ */
+
+public Book{
+	String title;
+	int pageCount;
+
+	public Book(String title, int pageCount){
+		this.title = title;
+		this.pageCount = pageCount;
+	}
+	
+}
